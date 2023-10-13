68970fe2560e05e9c24c3c9496975eb4b28f391f
Dysivet-odoo <dysprosium.239@gmail.com>
2023-10-13 11:31:35 +0300

    Add create partners

--- sync/models/sync_partner.py
+++ sync/models/sync_partner.py
@@ -18,21 +18,21 @@ class SyncPartner(models.Model):
         existing = self._search_partner(bot_id, external_id)
         if existing:
             return existing
-        vals = {'partner_id':partner_id,'bot_id':bot_id,'external_id':external_id}
+        vals = {'partner_id': partner_id, 'bot_id': bot_id, 'external_id': external_id}
         return self.create(vals)
 
     def _search_partner(self, bot_id, external_id):
-        domain = [('bot_id','=',bot_id),('external_id','=',external_id)]
+        domain = [('bot_id', '=', bot_id), ('external_id', '=', external_id)]
         return self.search(domain)
 
     def get_partner(self, bot_id, external_id):
         users = self._search_partner(bot_id, external_id)
         if len(users) > 1:
             raise ValidationError(
-                    (
-                        "Sync_partner get_user found multiple links"
-                    )
+                (
+                    "Sync_partner get_user found multiple links"
                 )
+            )
         return users.odoo if users else None
 
     def get_partner_with_create(self, bot_id, external_id, callback_func=None, callback_kwargs=None):
@@ -53,7 +53,38 @@ class SyncPartner(models.Model):
         return f
 
     def set_state(self, partner_id, bot_id, state):
+        print('------------------------')
         user = self.search([('bot_id', '=', bot_id), ('partner_id', '=', partner_id)])
+        if not user:
+            return
+        if user.state_user == 'phone' and state == 'None':
+            child_partner = self.env['res.partner'].browse(partner_id)
+            print(child_partner)
+            partners = self.env['res.partner'].search(
+                [('phone', '=', child_partner.phone),('id', '!=', partner_id)])
+            print(partners)
+            is_parent_exist = False
+            parent_part_id = None
+            for p in partners:
+                if p.parent_id:
+                    is_parent_exist = True
+                    parent_part_id = p.parent_id
+                    break
+            if not is_parent_exist:
+                list_name = child_partner.name.split(' ')
+                list_name.pop()
+                name = ''.join(list_name)
+                parent_partner = self.env['res.partner'].create({'name': name, 'phone': child_partner.phone})
+                parent_part_id = parent_partner.id
+
+            for p in partners:
+                p.parent_id = parent_part_id
+            child_partner.parent_id = parent_part_id
+            # for p in partners:
+            #     if not p.parent_id:
+            #         child_partner.parent_id = p.id
+            #         print(p)
+            #         break
         user.state_user = state
 
     def get_state(self, partner_id, bot_id):
@@ -62,7 +93,7 @@ class SyncPartner(models.Model):
 
     def _get_eval_context(self):
         return {
-            'get_partner_with_create':self.get_partner_with_create,
-            'get_state':self.get_state,
-            'set_state':self.set_state,
+            'get_partner_with_create': self.get_partner_with_create,
+            'get_state': self.get_state,
+            'set_state': self.set_state,
         }
