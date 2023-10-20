ecaee342ab297834a8605f03d3262029c74b3e3c
max-zagrebelny <maksimzagrebelnij5@gmail.com>
2023-10-20 15:25:47 +0300

    Add chatter button

---
+++ sync_viber/static/src/models/chatter_topbar.js
@@ -0,0 +1,16 @@
+/** @odoo-module **/
+
+import { registerPatch } from '@mail/model/model_core';
+import { clear } from '@mail/model/model_field_command';
+import '@mail/models/chatter_topbar';
+
+registerPatch({
+    name: 'ChatterTopbar',
+    recordMethods: {
+        async onClick() {
+          this.chatter.thread.open();
+          let otherChannelMembers = this.chatter.thread.suggestedRecipientInfoList;
+          console.log(otherChannelMembers);
+        },
+    },
+});
