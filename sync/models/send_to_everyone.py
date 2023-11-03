61b85a3ddc0e18965a05fe7f25a8e943ca0e41fd
Dysivet-odoo <dysprosium.239@gmail.com>
2023-11-03 12:33:42 +0200

    Modified megre contacts

--- sync/models/send_to_everyone.py
+++ sync/models/send_to_everyone.py
@@ -7,6 +7,15 @@ class SendToEveryone(models.Model):
 
     msg = fields.Text("Message")
     project_id = fields.Many2one("sync.project")
+    bot_name = fields.Char('Bot Name', related='project_id.name')
+
+    # sync_partners_ids = fields.Many2many("sync.partner","sync_partner_rel","send_to_everyone_id","sync_partner_id", string='Partners')
+    # bot_name = fields.Char('Bot Name', related='project_id.name')
+    #
+    # @api.depends('project_id')
+    # def test(self):
+    #     for record in self:
+    #         self.write({'sync_partners_ids':record.project_id.user_ids})
 
 
 class SendToEveryoneViber(models.Model):
